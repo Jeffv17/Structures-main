cbc07eeba3b3fcdaade51b6fb7fc4acea5b4b0f8
Jeffrey Voltaire <Jeffreyv300@gmail.com>
2021-09-28 08:45:46 -0400

    sss

--- Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
+++ Lab - Structures.playground/Pages/1. Exercise - Structs, Instances, and Default Values.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
  
  Imagine you are creating an app that will monitor location. Create a `GPS` struct with two variable properties, `latitude` and `longitude`, both with default values of 0.0.
  */
-struct GPS{
+ struct GPS{
     var latitude = 0.0
     var longitude = 0.0
 }
@@ -11,13 +11,13 @@ struct GPS{
 /*:
  Create a variable instance of `GPS` called `somePlace`. It should be initialized without supplying any arguments. Print out the latitude and longitude of `somePlace`, which should be 0.0 for both.
  */
-    var somePlace: String
+var somePlace = GPS(latitude: 0.0, longitude: 0.0)
 
 /*:
  Change `somePlace`'s latitude to 51.514004, and the longitude to 0.125226, then print the updated values.
  */
-
-
+somePlace.latitude = 51.514004
+somePlace.longitude = 0.125226
 /*:
  Now imagine you are making a social app for sharing your favorite books. Create a `Book` struct with four variable properties: `title`, `author`, `pages`, and `price`. The default values for both `title` and `author` should be an empty string. `pages` should default to 0, and `price` should default to 0.0.
  */
